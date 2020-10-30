6b0248711c29758c4827287e3516b5ec019f2fc9
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2020-10-30 13:50:56 -0700

    feat: guess random number

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)
+    path("", views.index),
+    path("submit_guess", views.submit_guess),
+    path("result", views.result)
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from json import dumps
+import random
 
 
 def index(request):
-    return render(request, 'index.html')
+    request.session['random'] = random.randint(1, 100)
+    print(request.session['random'])
+    context = {
+        "guess_result": ""
+    }
+    js_data = dumps(context)
+    return render(request, 'index.html', {"data": js_data})
+
+
+def submit_guess(request):
+    if request.POST['guess']:
+        request.session['guess'] = request.POST['guess']
+        return redirect("/result")
+    else:
+        return redirect('/')
+
+
+def result(request):
+    context = {
+        "guess_result": ""
+    }
+    if int(request.session['guess']) < request.session['random']:
+        context['guess_result'] = "<div id='result' class='bg-danger'>" + \
+            "Too Low!</div>"
+    elif int(request.session['guess']) > request.session['random']:
+        context['guess_result'] = "<div id='result' class='bg-danger'>" + \
+            "Too High!</div>"
+    else:
+        button = "<button onclick=\"window.location.href='/'\">Play Again!</button>"
+        context['guess_result'] = "<div id='result' class='bg-success'>" + \
+            request.session['guess']+" Was the Number!"+button+"</div>"
+    js_data = dumps(context)
+    return render(request, 'index.html', {"data": js_data})
